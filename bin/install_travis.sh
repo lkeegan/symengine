4376893997bb6898babecc4d4771f17fb29d3e14
Liam Keegan <liam@keegan.ch>
2020-09-20 16:16:04 +0200

    migrate bench / bench_eval_double to google benchmark
    - remove nonius from CMakeLists, source code
    - update README
    - update CI to build google benchmarks

--- bin/install_travis.sh
+++ bin/install_travis.sh
@@ -127,6 +127,10 @@ elif [[ "${WITH_LLVM}" == "8.0" ]]; then
     export LLVM_DIR=/usr/lib/llvm-8/share/llvm/
 elif [[ "${WITH_LLVM}" == "6.0" ]]; then
     export LLVM_DIR=/usr/lib/llvm-6.0/share/llvm/
+elif [[ "${WITH_LLVM}" == "12" ]]; then
+    export LLVM_DIR=/usr/lib/llvm-12/share/llvm/
+    export CC=clang-12
+    export CXX=clang++-12
 elif [[ ! -z "${WITH_LLVM}" ]]; then
     conda_pkgs="$conda_pkgs llvmdev=${WITH_LLVM} cmake=3.10.0"
     export LLVM_DIR=$our_install_dir/share/llvm/
