fcbbde38d9145a4b15187d7335d7e37b97ef0734
Liam Keegan <liam@keegan.ch>
2021-05-04 16:30:49 +0200

    add partial mathml -> infix parser
    - convert mathml to infix string, then parse that
    
    some open questions:
    
    - is this a sane approach? some info may be lost in round trip, e.g. maybe real(1.0) -> integer(1)
    - seems like it might be better to visit the xml & directly construct symengine objects from that
    - which mathml version(s) to support?
    - does/should the infix parser support piecewise/contains?

--- symengine/parser/mathml_parser.cpp
+++ symengine/parser/mathml_parser.cpp
@@ -1,4 +1,5 @@
 #include <tinyxml2.h>
+#include <cstring>
 #include <symengine/parser/mathml_parser.h>
 #include <symengine/real_double.h>
 #include <symengine/real_mpfr.h>
@@ -8,6 +9,145 @@
 namespace SymEngine
 {
 
+class MathmlInfixVisitor : public tinyxml2::XMLVisitor
+{
+private:
+    std::size_t depth{0};
+    std::vector<char> op;
+    std::string sep{};
+    std::vector<bool> print_op;
+    std::map<std::string, std::string> func_names;
+    std::map<std::string, std::string> constant_names;
+    std::map<std::string, char> ops;
+
+public:
+    std::stringstream s;
+    MathmlInfixVisitor() : op(1024, ','), print_op(1024, false)
+    {
+        ops["plus"] = '+';
+        ops["minus"] = '-';
+        ops["times"] = '*';
+        ops["divide"] = '/';
+
+        func_names["power"] = "pow";
+        func_names["arcsin"] = "asin";
+        func_names["arccos"] = "acos";
+        func_names["arcsec"] = "asec";
+        func_names["arccsc"] = "acsc";
+        func_names["arctan"] = "atan";
+        func_names["arccot"] = "acot";
+        func_names["arcsinh"] = "asinh";
+        func_names["arccsch"] = "acsch";
+        func_names["arccosh"] = "acosh";
+        func_names["arctanh"] = "atanh";
+        func_names["arccoth"] = "acoth";
+        func_names["arcsech"] = "asech";
+        func_names["lt"] = "Lt";
+        func_names["leq"] = "Le";
+        func_names["gt"] = "Gt";
+        func_names["geq"] = "Geq";
+        func_names["eq"] = "Eq";
+        func_names["and"] = "And";
+        func_names["or"] = "Or";
+        func_names["xor"] = "Xor";
+        func_names["not"] = "Not";
+
+        constant_names["true"] = "True";
+        constant_names["false"] = "False";
+        constant_names["pi"] = "pi";
+        constant_names["exponentiale"] = "E";
+        constant_names["eulergamma"] = "EulerGamma";
+    }
+
+    bool VisitEnter(const tinyxml2::XMLElement &element,
+                    const tinyxml2::XMLAttribute *attribute) override
+    {
+        std::string name{element.Name()};
+        std::cout << "Enter " << name << std::endl;
+        if (attribute != nullptr) {
+            std::cout << "  - " << attribute->Name() << std::endl;
+        }
+        if (name == "apply") {
+            // next element is operator
+            return true;
+        }
+        if (name == "cn") {
+            // number
+            const auto *att{attribute};
+            while (att != nullptr) {
+                if (strcmp(att->Name(), "type") == 0) {
+                    if (strcmp(attribute->Value(), "complex-cartesian") == 0) {
+                        sep = " + I*";
+                    } else if (strcmp(attribute->Value(), "rational") == 0) {
+                        sep = "/";
+                    }
+                }
+                att = att->Next();
+            }
+            return true;
+        }
+        if (name == "ci") {
+            // symbol
+            return true;
+        }
+        if (name == "sep") {
+            // separator in complex/rational number
+            print_op[depth] = false;
+            s << sep;
+            return true;
+        }
+        // built-in constant
+        auto ic = constant_names.find(name);
+        if (ic != constant_names.cend()) {
+            s << ic->second;
+            return true;
+        }
+        // built-in function
+        if (print_op[depth]) {
+            s << ' ' << op[depth] << ' ';
+        }
+        print_op[depth] = true;
+        ++depth;
+        std::cout << "depth " << depth << std::endl;
+        auto iop = ops.find(name);
+        if (iop != ops.cend()) {
+            op[depth] = iop->second;
+            s << "(";
+            return true;
+        }
+        op[depth] = ',';
+        auto i_func = func_names.find(name);
+        if (i_func != func_names.cend()) {
+            s << i_func->second << "(";
+            return true;
+        }
+        // otherwise assume user-defined function for now
+        s << element.Name() << "(";
+        return true;
+    }
+
+    bool VisitExit(const tinyxml2::XMLElement &element) override
+    {
+        std::string name{element.Name()};
+        std::cout << "Exit " << name << std::endl;
+        if (name == "apply") {
+            print_op[depth] = false;
+            --depth;
+            s << ")";
+        }
+        return true;
+    }
+    bool Visit(const tinyxml2::XMLText &text) override
+    {
+        if (print_op[depth]) {
+            s << ' ' << op[depth] << ' ';
+        }
+        print_op[depth] = true;
+        s << text.Value();
+        return true;
+    }
+};
+
 RCP<const Basic> parse_mathml(const std::string &xml)
 {
     MathmlParser p{};
@@ -16,7 +156,14 @@ RCP<const Basic> parse_mathml(const std::string &xml)
 
 RCP<const Basic> MathmlParser::parse(const std::string &xml)
 {
-    return integer(0);
+    tinyxml2::XMLDocument doc;
+    std::cout << xml << std::endl;
+    doc.Parse(xml.c_str());
+    MathmlInfixVisitor v;
+    doc.Accept(&v);
+    std::string expr{v.s.str()};
+    std::cout << "EXPR: " << expr << std::endl;
+    return SymEngine::parse(expr);
 }
 
 MathmlParser::MathmlParser() = default;
--- symengine/tests/basic/test_mathml_parser.cpp
+++ symengine/tests/basic/test_mathml_parser.cpp
@@ -18,6 +18,7 @@ using SymEngine::E;
 using SymEngine::Eq;
 using SymEngine::erf;
 using SymEngine::erfc;
+using SymEngine::EulerGamma;
 using SymEngine::from_basic;
 using SymEngine::function_symbol;
 using SymEngine::gamma;
@@ -40,6 +41,7 @@ using SymEngine::logical_xnor;
 using SymEngine::logical_xor;
 using SymEngine::Lt;
 using SymEngine::make_rcp;
+using SymEngine::mathml;
 using SymEngine::max;
 using SymEngine::min;
 using SymEngine::minus_one;
@@ -47,9 +49,11 @@ using SymEngine::Mul;
 using SymEngine::Ne;
 using SymEngine::Number;
 using SymEngine::one;
+using SymEngine::parse;
 using SymEngine::parse_mathml;
 using SymEngine::ParseError;
 using SymEngine::pi;
+using SymEngine::piecewise;
 using SymEngine::pow;
 using SymEngine::Rational;
 using SymEngine::rational;
@@ -64,8 +68,143 @@ using SymEngine::zero;
 
 using namespace SymEngine::literals;
 
-TEST_CASE("Mathml Parsing: internal data structures", "[mathml_parser]")
+TEST_CASE("Mathml parsing roundtrips", "[mathml_parser]")
 {
-    std::string s;
-    REQUIRE(eq(*parse_mathml(s), *integer(0)));
+    for (const std::string &str :
+         {"1",
+          "1.2",
+          "1e-5",
+          "pi",
+          "E",
+          "EulerGamma",
+          "x",
+          "x+y",
+          "x+y+z",
+          "x-y",
+          "x-y-z",
+          "x*y",
+          "x*y*z",
+          "X/Y",
+          "X/Y/Z",
+          "1/3",
+          "1/3/2",
+          "x**2",
+          "x**2**3",
+          "(x/3+y)**3",
+          "a + c*(b*x)+y/z",
+          "a + atanh(cos(1+c*(b*x))+sin(tan(y/z)))",
+          "csc(2-cot(1+sec(x)))",
+          "sinh(cosh(y)+tanh(qq))",
+          "csch(2-coth(1+sech(x)))",
+          "atan2(y, z)",
+          "acos(asin(cos(atan(3) + sin(x+y))))",
+          "atanh(acoth(x))+acsc(y*asin(z))+acos(w*asec(w*acsc(q)))",
+          "asech(1+acot(asinh(x)+acsch(y)-acosh(z)+atanh(z))+acoth(z))",
+          "log(x)",
+          "ln(x)",
+          "log(x, y)",
+          "floor(x)",
+          "ceiling(y)",
+          "min(a, b)",
+          "max(a, b)",
+          "abs(x)",
+          "x>3",
+          "x>=3",
+          "y<2",
+          "y<=x",
+          "y==q",
+          "x<3 & y>2",
+          "x<3 | y>2",
+          "Xor(x<3 , y>2)",
+          "Xnor(x<3 , y>2)",
+          "Nor(x<3 , y>2)",
+          "True",
+          "False",
+          "Not(True)",
+          "Not(x<3)"}) {
+        // parse infix
+        auto expr = parse(str);
+        // print as mathml
+        auto xml = mathml(*expr);
+        // parse mathml
+        auto res = parse_mathml(xml);
+        CAPTURE(str);
+        CAPTURE(*expr);
+        CAPTURE(xml);
+        CAPTURE(*res);
+        REQUIRE(eq(*expr, *res));
+    }
+}
+
+TEST_CASE("Mathml parsing numbers", "[mathml_parser]")
+{
+    RCP<const Basic> res;
+    RCP<const Basic> expr;
+
+    res = parse_mathml("<cn>12345</cn>");
+    expr = integer(12345);
+    REQUIRE(eq(*res, *expr));
+    REQUIRE(eq(*parse_mathml(mathml(*res)), *expr));
+
+    res = parse_mathml("<cn type=\"rational\"> 12342 <sep/> 2342342 </cn>");
+    expr = rational(12342, 2342342);
+    REQUIRE(eq(*res, *expr));
+    REQUIRE(eq(*parse_mathml(mathml(*res)), *expr));
+
+    res = parse_mathml("<cn type=\"complex-cartesian\">3<sep/>4</cn>");
+    expr = Complex::from_two_nums(*integer(3), *integer(4));
+    REQUIRE(eq(*res, *expr));
+    // todo: mathml prints a custom complex type
+    // REQUIRE(eq(*parse_mathml(mathml(*res)), *expr));
+}
+
+TEST_CASE("Mathml parsing constants", "[mathml_parser]")
+{
+    RCP<const Basic> res;
+    RCP<const Basic> expr;
+
+    res = parse_mathml("<true />");
+    expr = boolTrue;
+    REQUIRE(eq(*res, *expr));
+    REQUIRE(eq(*parse_mathml(mathml(*res)), *expr));
+
+    res = parse_mathml("<false />");
+    expr = boolFalse;
+    REQUIRE(eq(*res, *expr));
+    REQUIRE(eq(*parse_mathml(mathml(*res)), *expr));
+
+    res = parse_mathml("<pi />");
+    expr = pi;
+    REQUIRE(eq(*res, *expr));
+    REQUIRE(eq(*parse_mathml(mathml(*res)), *expr));
+
+    res = parse_mathml("<exponentiale />");
+    expr = E;
+    REQUIRE(eq(*res, *expr));
+    REQUIRE(eq(*parse_mathml(mathml(*res)), *expr));
+
+    res = parse_mathml("<eulergamma />");
+    expr = EulerGamma;
+    REQUIRE(eq(*res, *expr));
+    REQUIRE(eq(*parse_mathml(mathml(*res)), *expr));
+}
+
+TEST_CASE("Mathml parsing piecewise", "[mathml_parser]")
+{
+    RCP<const Basic> res;
+    RCP<const Basic> expr;
+
+    auto x = symbol("x");
+    auto y = symbol("y");
+    auto int1 = interval(integer(1), integer(2), true, false);
+    auto int2 = interval(integer(2), integer(5), true, false);
+    auto int3 = interval(integer(5), integer(10), true, false);
+    auto p = piecewise({{x, contains(x, int1)},
+                        {y, contains(x, int2)},
+                        {add(x, y), contains(x, int3)}});
+
+    expr = p;
+    res = parse_mathml(mathml(*expr));
+    REQUIRE(eq(*res, *expr));
+    REQUIRE(eq(*parse_mathml(mathml(*res)), *expr));
 }
